66f54be9c15040526e3bd2c693cf55c3e5e8c184
jonas_ayres <jonas.ayres@southsystem.com.br>
2019-07-22 11:35:36 -0300

    add rest server and server start to main app file

--- src/main/java/com/pragma/brewery/Application.java
+++ src/main/java/com/pragma/brewery/Application.java
@@ -1,7 +1,13 @@
 package com.pragma.brewery;
 
+import java.io.IOException;
+
+import com.pragma.brewery.rest.RestServer;
+
+import com.sun.net.httpserver.HttpServer;
+
 public class Application {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
+  public static void main(String[] args) throws IOException {
+    RestServer.buildAndReturnHttpServer().start();
   }
 }
---
+++ src/main/java/com/pragma/brewery/rest/RestServer.java
@@ -0,0 +1,38 @@
+package com.pragma.brewery.rest;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpServer;
+
+public class RestServer {
+  private static final BeerResource beerResource = new BeerResource();
+  private static final int SERVER_PORT = 8000;
+  private static HttpServer httpServer;
+
+  private static final String BASE_URI = "/api";
+  private static final String BEERS_URI = BASE_URI + "/beers";
+  private static final String MONITORING_URI = BASE_URI + "/monitoring";
+
+  private static void createContexts() {
+    httpServer.createContext(BEERS_URI, (exchange -> {
+      beerResource.handleBeerCRUDExchange(exchange);
+    }));
+    httpServer.createContext(MONITORING_URI, (exchange -> {
+      beerResource.handleBeerMonitoringExchange(exchange);
+    }));
+  }
+
+  public static HttpServer buildAndReturnHttpServer() throws IOException {
+    try {
+      httpServer = HttpServer.create(new InetSocketAddress(SERVER_PORT), 0);
+      createContexts();
+      httpServer.setExecutor(null);
+    }
+    catch(Exception e) {
+      System.err.println(e);
+    }
+
+    return httpServer;
+  }
+}
